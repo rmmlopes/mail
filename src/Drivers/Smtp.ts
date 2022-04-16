5d4260da813101c91dca9a4d8841e9a9ef483d5b
rmmlopes <rui@ruiandjenn.ca>
2022-04-15 23:49:48 -0600

    Update Smtp.ts
    Nodemailer is not setting the headers in the email when headers are provided in Adonis format.  
    This converts the format from Adonis to Nodemailer format

--- src/Drivers/Smtp.ts
+++ src/Drivers/Smtp.ts
@@ -34,7 +34,9 @@ export class SmtpDriver implements SmtpDriverContract {
     if (!this.transporter) {
       throw new Error('Driver transport has been closed and cannot be used for sending emails')
     }
-
+    
+    message.headers = this.convertHeadersToNodemailFormat(message.headers)
+    
     return this.transporter.sendMail(message)
   }
 
@@ -44,5 +46,34 @@ export class SmtpDriver implements SmtpDriverContract {
   public async close() {
     this.transporter.close()
     this.transporter = null
+  }
+  
+  /**
+   * nodemailer expects an object for headers, Adonis mailer uses an an array of objects
+   *   headers: [ { 'x-test': 'teststr' }, { 'x-test': '1234' } ]  ===>
+   *   headers: { 'x-test': [ 'teststr', '1234' ] }
+   */
+  public convertHeadersToNodemailFormat(headers) {
+    if (headers) {
+      var newHeaders = {}
+      console.log(headers)
+      for (var header of headers) {
+        console.log(header)
+        for (var headerKey in header) {
+          if (!newHeaders[headerKey]) {
+            newHeaders[headerKey] = []
+          }
+          if (header[headerKey] instanceof Array) {
+            for (var headerListVal of header[headerKey]) {
+              newHeaders[headerKey].push(headerListVal)
+            }
+          } else {
+            newHeaders[headerKey].push(header[headerKey])
+          }
+        }
+      }
+      return newHeaders
+    }
+    return headers
   }
 }
